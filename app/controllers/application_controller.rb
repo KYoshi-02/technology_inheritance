3bb2aa787dc90692e2a10d698e222e3a4de628c8
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-12 21:59:30 +0900

    ログイン機能(仮)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
+
   private
 
   def basic_auth
