3350730f80a43b190372cdb7f0f452ffcb95c0b8
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-19 19:23:11 +0900

    Knowhowpostsコントローラー修正

--- app/controllers/knowhow_posts_controller.rb
+++ app/controllers/knowhow_posts_controller.rb
@@ -1,65 +1,55 @@
 class KnowhowPostsController < ApplicationController
-  before_action :set_knowhow_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_knowhow_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
-  # GET /knowhow_posts or /knowhow_posts.json
   def index
-    @knowhow_posts = KnowhowPost.all
+    @knowhow_posts = current_user.company.knowhow_posts.includes(:user)
   end
 
-  # GET /knowhow_posts/1 or /knowhow_posts/1.json
-  def show
-  end
+  def show; end
 
-  # GET /knowhow_posts/new
   def new
-    @knowhow_post = KnowhowPost.new
+    @knowhow_post = current_user.knowhow_posts.build
   end
 
-  # GET /knowhow_posts/1/edit
-  def edit
-  end
-
-  # POST /knowhow_posts or /knowhow_posts.json
   def create
     @knowhow_post = current_user.knowhow_posts.build(knowhow_post_params)
     if @knowhow_post.save
-      redirect_to @knowhow_post, notice: "ノウハウを投稿しました。"
+      redirect_to @knowhow_post, notice: '投稿が完了しました'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /knowhow_posts/1 or /knowhow_posts/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @knowhow_post.update(knowhow_post_params)
-        format.html { redirect_to @knowhow_post, notice: "Knowhow post was successfully updated." }
-        format.json { render :show, status: :ok, location: @knowhow_post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @knowhow_post.errors, status: :unprocessable_entity }
-      end
+    if @knowhow_post.update(knowhow_post_params)
+      redirect_to @knowhow_post, notice: '投稿を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /knowhow_posts/1 or /knowhow_posts/1.json
   def destroy
-    @knowhow_post.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to knowhow_posts_path, status: :see_other, notice: "Knowhow post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @knowhow_post.destroy
+    redirect_to knowhow_posts_path, notice: '投稿を削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_knowhow_post
-      @knowhow_post = KnowhowPost.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def knowhow_post_params
-      params.require(:knowhow_post).permit(:title, :content, :image, :video)
+  def set_knowhow_post
+    @knowhow_post = KnowhowPost.find(params[:id])
+  end
+
+  def knowhow_post_params
+    params.require(:knowhow_post).permit(:title, :body)
+  end
+
+  def authorize_user!
+    unless @knowhow_post.user == current_user || current_user.admin?
+      redirect_to root_path, alert: '権限がありません'
     end
+  end
 end
