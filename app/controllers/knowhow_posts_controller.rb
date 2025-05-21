4df1d5e2889776b3bd7af06366ddd5c5509e15bb
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-21 19:11:34 +0900

    修正

--- app/controllers/knowhow_posts_controller.rb
+++ app/controllers/knowhow_posts_controller.rb
@@ -1,22 +1,21 @@
 class KnowhowPostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_knowhow_post, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
-    @knowhow_posts = current_user.company.knowhow_posts.includes(:user)
+    @knowhow_posts = KnowhowPost.includes(:user).where(user: { company_id: current_user.company_id }).order(created_at: :desc)
   end
 
   def show; end
 
   def new
-    @knowhow_post = current_user.knowhow_posts.build
+    @knowhow_post = KnowhowPost.new
   end
 
   def create
     @knowhow_post = current_user.knowhow_posts.build(knowhow_post_params)
     if @knowhow_post.save
-      redirect_to @knowhow_post, notice: '投稿が完了しました'
+      redirect_to @knowhow_post, notice: "投稿が完了しました"
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,7 +25,7 @@ class KnowhowPostsController < ApplicationController
 
   def update
     if @knowhow_post.update(knowhow_post_params)
-      redirect_to @knowhow_post, notice: '投稿を更新しました'
+      redirect_to @knowhow_post, notice: "更新が完了しました"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -34,7 +33,7 @@ class KnowhowPostsController < ApplicationController
 
   def destroy
     @knowhow_post.destroy
-    redirect_to knowhow_posts_path, notice: '投稿を削除しました'
+    redirect_to knowhow_posts_path, notice: "削除しました"
   end
 
   private
@@ -44,12 +43,6 @@ class KnowhowPostsController < ApplicationController
   end
 
   def knowhow_post_params
-    params.require(:knowhow_post).permit(:title, :body)
-  end
-
-  def authorize_user!
-    unless @knowhow_post.user == current_user || current_user.admin?
-      redirect_to root_path, alert: '権限がありません'
-    end
+    params.require(:knowhow_post).permit(:title, :content, :image, :video)
   end
 end
--- app/models/knowhow_post.rb
+++ app/models/knowhow_post.rb
@@ -1,3 +1,9 @@
 class KnowhowPost < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :image
+  has_one_attached :video
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
