554875ea1ec87b9de8142b3303cc7ad299f7f3fe
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-29 07:09:11 +0900

    フロント修正

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,8 +1,9 @@
 class QuizzesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_quiz, only: [:show]
 
   def index
-    @quizzes = Quiz.all
+    @quizzes = Quiz.all.order(created_at: :desc)
   end
 
   def new
@@ -10,15 +11,16 @@ class QuizzesController < ApplicationController
   end
 
   def create
-    @quiz = Quiz.new(quiz_params)
+    @quiz = current_user.quizzes.build(quiz_params)
     if @quiz.save
-      redirect_to @quiz, notice: "クイズを作成しました"
+      redirect_to @quiz, notice: "クイズを投稿しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
+    @quiz = Quiz.find(params[:id])
   end
 
   private
@@ -28,6 +30,6 @@ class QuizzesController < ApplicationController
   end
 
   def quiz_params
-    params.require(:quiz).permit(:question, :correct_answer)
+    params.require(:quiz).permit(:question, :correct_answer, :explanation)
   end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,6 @@ class Quiz < ApplicationRecord
   belongs_to :user
   belongs_to :company
   has_many :answers, dependent: :destroy
+
+  validates :question, :correct_answer, presence: true
 end
